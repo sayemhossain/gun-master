d1e44ec2b21399d26f89b87ca27ef0bd182b2ea2
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-03-25 04:26:52 +0600

    new code

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -3,22 +3,26 @@ import "./Card.css";
 
 const Card = (props) => {
   console.log(props);
-  const { img } = props.product;
+  const { img, name, action, bullet, category, price } = props.product;
   return (
     <div className="card-container my-5">
-      <div className="card">
+      <div className="card text-center ">
         <div className="image-container">
           <img className="img-fluid" src={img} alt="" />
         </div>
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
+          <h5 className="card-title ">{name}</h5>
+          <p className="my-1 text-success fw-bold">{category}</p>
+          <div className="my-4">
+            <p className="my-1">Action: {action}</p>
+            <p className="my-1">Bullet: {bullet}</p>
+            <p className="my-1">Price: ${price}</p>
+          </div>
+          <div className="">
+            <a href="#" className="btn btn-dark ">
+              Add to Cart
+            </a>
+          </div>
         </div>
       </div>
     </div>
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container">
           <a className="navbar-brand" href="#">
-            <span className="text-warning">GAN</span> MASTER
+            <span className="text-warning">GUN</span> MASTER
           </a>
           <button
             className="navbar-toggler"
